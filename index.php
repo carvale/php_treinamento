de99ba250ba41465b4b1a14912b8befed3c67219
Luiz Carvalho <carvale71@yahoo.com.br>
2020-09-20 19:18:52 -0300

    Adm Login

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+session_start();
+require_once("vendor/autoload.php");
+require_once("functions.php");
+
+use Hcode\Model\User;
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$page = new Hcode\Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	User::verifyLogin();
+
+	$page = new Hcode\PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /ecommerce/admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /ecommerce/admin/login");
+	exit;
+
+});
+
+$app->run();
+
+ ?>
